4659744bf32c6c51df5e7fd85954fee23f947732
任振飞 <renzhenfei@renzheneidembp2.lan>
2017-04-16 21:00:12 +0800

    新建的测试代码

---
+++ ZFTest/ZFTestTests/ZFTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZFTestTests.swift
+//  ZFTestTests
+//
+//  Created by 任振飞 on 2017/4/16.
+//  Copyright © 2017年 任振飞. All rights reserved.
+//
+
+import XCTest
+@testable import ZFTest
+
+class ZFTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

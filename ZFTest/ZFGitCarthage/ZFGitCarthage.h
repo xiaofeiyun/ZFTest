b588cde1c11cf89389b82c707991a5dc7fff7789
任振飞 <89412859@qq.com>
2017-07-18 19:10:31 +0800

    test Carthage

---
+++ ZFTest/ZFGitCarthage/ZFGitCarthage.h
@@ -0,0 +1,19 @@
+//
+//  ZFGitCarthage.h
+//  ZFGitCarthage
+//
+//  Created by 任振飞 on 2017/7/18.
+//  Copyright © 2017年 任振飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZFGitCarthage.
+FOUNDATION_EXPORT double ZFGitCarthageVersionNumber;
+
+//! Project version string for ZFGitCarthage.
+FOUNDATION_EXPORT const unsigned char ZFGitCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZFGitCarthage/PublicHeader.h>
+
+
